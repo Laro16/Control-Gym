815998a5d62268a6698506437702ebb240cdbe9a
Laro16 <luis21aro@gmail.com>
2026-08-02 18:25:12 -0600

    Delete UserPayments.jsx

--- UserPayments.jsx
+++
@@ -1,4 +0,0 @@
-// Este archivo re-exporta los dashboards desde sus módulos propios.
-// Mantenerlo aquí para compatibilidad con App.jsx sin cambiar nada.
-export { AdminDashboard } from './AdminDashboard'
-export { UserDashboard }  from './UserDashboard'
